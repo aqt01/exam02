842f6e53a082b7f28e0a5b50f585e2f550367a07
aqt01 <ofoi@localhost.localdomain>
2014-07-15 10:31:53 -0400

    models relationship created

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,11 @@
 class Teacher < ActiveRecord::Base
+	has_many :subjects
+	accepts_nested_attributes_for :subjects
+
+#        def active_teachers_subjects
+#		for
+#                sum = subjects.size
+#                return sum
+#        end
+
 end
